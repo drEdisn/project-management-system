b38bbb8e553dc722b525c1b8236d80da96928141
drEdisn <denizs00@mail.ru>
2022-09-30 16:36:06 +0300

    feat: add env

--- manage-app/server.js
+++ manage-app/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const server = express();
+require('dotenv').config();
+const port = process.env.SERVER_PORT || 4200;
 
 server.use(express.static(__dirname + '/dist/manage-app'));
 server.get('/*', (_, res) => res.sendFile(path.join(__dirname + '/dist/manage-app/index.html')));
 
-server.listen(4200);
+server.listen(port, () => console.log(`Server started in http://localhost:${port}`));
